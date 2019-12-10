b944618974d70f516a7a8cc9d64fb0912d1cb52b
Atikur-Rahman-Sabuj <sabuj.kucse@gmail.com>
2019-12-10 18:35:31 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
-
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations:[
+    trigger('myInsertRemoveTrigger', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('.5s', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('.5s', style({ opacity: 0 }))
+      ])
+    ]),
+  ]
 })
 export class AppComponent {
   title = 'ToDoList';
+  public ToDoItem: String = "test";
+  public ToDoItems : String[]=[];
+  public EditTarget : String[]=[];
+  public IsEditing : Boolean[]=[];
+  public SaveItem(){
+    //console.log(this.ToDoItem);
+    this.ToDoItems.push(this.ToDoItem);
+  }
+  public EditItem(index){
+    if (this.IsEditing[index]) {
+      this.IsEditing[index] = false;
+    }else{
+      this.EditTarget[index] = this.ToDoItems[index];
+      this.IsEditing[index] = true;
+    }
+    
+  }
+  public CompleteEditItem(index){
+    this.ToDoItems[index] = this.EditTarget[index];
+    this.IsEditing[index] = false;
+    console.log(this.ToDoItems[index])
+  }
+  public DeleteItem(index){
+    this.ToDoItems.splice(index,1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
